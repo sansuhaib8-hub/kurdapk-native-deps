ee1d5cfba7de94161de39a291513406ffc8798b5
sansuhaib8-hub <sansuhaib8@gmail.com>
2026-08-31 19:10:41 +0300

    Add step-by-step debug logging before execv, write to app_flutter instead of /sdcard

--- exec-wrapper/glibcwrapper.c
+++ exec-wrapper/glibcwrapper.c
@@ -5,33 +5,38 @@
 #include <limits.h>
 #include <libgen.h>
 #include <errno.h>
-#include <sys/wait.h>
-#include <time.h>
 
 static void write_debug_log(const char *msg) {
-    FILE *f = fopen("/sdcard/impellerc_crash_debug.txt", "a");
+    FILE *f = fopen("/data/data/com.kurd.apkapp.kurdapk/app_flutter/impellerc_crash_debug.txt", "a");
     if (f) {
-        time_t t = time(NULL);
-        fprintf(f, "[%ld] %s\n", (long)t, msg);
+        fprintf(f, "%s\n", msg);
         fclose(f);
+    } else {
+        // Also try stderr in case terminal captures it
+        fprintf(stderr, "LOGFAIL: %s (errno=%d %s)\n", msg, errno, strerror(errno));
     }
 }
 
 int main(int argc, char *argv[]) {
+    write_debug_log("STEP1: main() entered");
+
     char self_path[PATH_MAX];
     ssize_t len = readlink("/proc/self/exe", self_path, sizeof(self_path) - 1);
     if (len == -1) { fprintf(stderr, "glibcwrapper: readlink failed: %s\n", strerror(errno)); return 127; }
     self_path[len] = '\0';
+    write_debug_log("STEP2: readlink done");
 
     char dir_buf[PATH_MAX];
     strncpy(dir_buf, self_path, sizeof(dir_buf) - 1);
     dir_buf[sizeof(dir_buf) - 1] = '\0';
     char *libdir = dirname(dir_buf);
+    write_debug_log("STEP3: dirname done");
 
     char base_buf[PATH_MAX];
     strncpy(base_buf, self_path, sizeof(base_buf) - 1);
     base_buf[sizeof(base_buf) - 1] = '\0';
     char *base = basename(base_buf);
+    write_debug_log("STEP4: basename done");
 
     char base_noext[PATH_MAX];
     strncpy(base_noext, base, sizeof(base_noext) - 1);
@@ -42,19 +47,17 @@ int main(int argc, char *argv[]) {
     }
 
     char real_target[PATH_MAX];
-    if (snprintf(real_target, sizeof(real_target), "%s/%s__real.so", libdir, base_noext) >= (int)sizeof(real_target)) {
-        fprintf(stderr, "glibcwrapper: path too long\n"); return 127;
-    }
+    snprintf(real_target, sizeof(real_target), "%s/%s__real.so", libdir, base_noext);
 
     char loader[PATH_MAX];
-    if (snprintf(loader, sizeof(loader), "%s/libglibc_ld.so", libdir) >= (int)sizeof(loader)) {
-        fprintf(stderr, "glibcwrapper: loader path too long\n"); return 127;
-    }
+    snprintf(loader, sizeof(loader), "%s/libglibc_ld.so", libdir);
+    write_debug_log("STEP5: paths built");
 
     unsetenv("LD_PRELOAD");
     char ld_path_env[PATH_MAX + 32];
     snprintf(ld_path_env, sizeof(ld_path_env), "LD_LIBRARY_PATH=%s", libdir);
     putenv(ld_path_env);
+    write_debug_log("STEP6: env set");
 
     char **new_argv = malloc(sizeof(char *) * (size_t)(argc + 4));
     if (new_argv == NULL) { fprintf(stderr, "glibcwrapper: out of memory\n"); return 127; }
@@ -64,43 +67,11 @@ int main(int argc, char *argv[]) {
     new_argv[3] = real_target;
     for (int i = 1; i < argc; i++) new_argv[i + 3] = argv[i];
     new_argv[argc + 3] = NULL;
+    write_debug_log("STEP7: argv built, about to execv");
 
-    char logmsg[512];
-    snprintf(logmsg, sizeof(logmsg), "About to fork+exec: loader=%s target=%s libdir=%s", loader, real_target, libdir);
-    write_debug_log(logmsg);
-
-    pid_t pid = fork();
-    if (pid == -1) {
-        write_debug_log("fork() failed");
-        fprintf(stderr, "glibcwrapper: fork failed: %s\n", strerror(errno));
-        free(new_argv);
-        return 127;
-    }
-
-    if (pid == 0) {
-        execv(loader, new_argv);
-        write_debug_log("execv failed in child");
-        fprintf(stderr, "glibcwrapper: execv(%s) failed: %s\n", loader, strerror(errno));
-        _exit(127);
-    }
-
-    int status;
-    waitpid(pid, &status, 0);
-    if (WIFEXITED(status)) {
-        snprintf(logmsg, sizeof(logmsg), "Child exited normally, code=%d", WEXITSTATUS(status));
-        write_debug_log(logmsg);
-        free(new_argv);
-        return WEXITSTATUS(status);
-    } else if (WIFSIGNALED(status)) {
-        int sig = WTERMSIG(status);
-        snprintf(logmsg, sizeof(logmsg), "Child KILLED by signal %d (%s)%s", sig, strsignal(sig),
-                 WCOREDUMP(status) ? " [core dumped]" : " [no core]");
-        write_debug_log(logmsg);
-        free(new_argv);
-        return 128 + sig;
-    } else {
-        write_debug_log("Child died with unknown status");
-        free(new_argv);
-        return 127;
-    }
+    execv(loader, new_argv);
+    write_debug_log("STEP8: execv RETURNED (this means execv FAILED)");
+    fprintf(stderr, "glibcwrapper: execv(%s) failed: %s\n", loader, strerror(errno));
+    free(new_argv);
+    return 127;
 }
